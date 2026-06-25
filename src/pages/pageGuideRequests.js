d735a6251db0377e578c9f4b5acbfd53f5e4e921
Rached SERGHINE <Rached.Serghine@prodigiup.com>
2026-06-25 15:32:08 +0200

    25/06

--- src/pages/pageGuideRequests.js
+++ src/pages/pageGuideRequests.js
@@ -73,35 +73,23 @@ const DivPageGuideRequests = () => {
           <tbody>
             <tr>
               <td style={{border:"1px solid black",textAlign:"left"}}>
-s
-                  <svg width="630" height="650" >
+
+                  <svg width="600" height="400" >
                 
                     <polygon points="153,60 223,85 153,110 83,85" fill={"orange"} />
                     <text x="153" y="90" textAnchor="middle" fill="black">Net User ?</text>
                     <line x1="220" y1="85" x2="263" y2="85" stroke="black" markerEnd="url(#arrow)" />
 
-                    <polygon points="353,55 443,85 353,115 263,85" fill={"orange"} />
-                    <text x="353" y="90" textAnchor="middle" fill="black">Need Approval ?</text>
+                    <polygon points="353,35 443,85 353,125 263,85" fill={"orange"} />
+                    <text x="353" y="77" textAnchor="middle" fill="black">User groups<tspan x="353" dy="1em">Instructions OK ?</tspan></text>
                     <line x1="441" y1="85" x2="471" y2="85" stroke="black" markerEnd="url(#arrow)" />
 
-                    <rect x="471" y="65" width="100" height="40" fill="lightgreen" stroke="black" />
-                    <text x="520" y="90" textAnchor="middle" fill="black" style={{ cursor: "pointer" }} >ADD USER</text>
 
-                    <line x1="353" y1="110" x2="353" y2="140" stroke="black" markerEnd="url(#arrow)" />
-                    <rect x="310" y="140" width="80" height="40" fill="cyan" stroke="black" />
-                    <text x="350" y="165" textAnchor="middle" fill="black" style={{ cursor: "pointer" }} >Send mail</text>
+                    <line x1="353" y1="124" x2="353" y2="308" stroke="black" markerEnd="url(#arrow)" />
 
-                    
-                    <polygon points="353,220 443,245 353,270 263,245" fill={"orange"} />
-                    <text x="353" y="250" textAnchor="middle" fill="black">Approved ?</text>
+                    <rect x="471" y="65" width="80" height="40" fill="red" stroke="black" />
+                    <text x="510" y="90" textAnchor="middle" fill="white" style={{ cursor: "pointer" }} >Reject</text>
 
-
-
-                    <line x1="441" y1="245" x2="471" y2="245" stroke="black" markerEnd="url(#arrow)" />                    
-                    <rect x="471" y="225" width="100" height="40" fill="red" stroke="black" />
-                    <text x="520" y="250" textAnchor="middle" fill="black" style={{ cursor: "pointer" }} >Reject</text>
-
-                    <line x1="353" y1="270" x2="353" y2="310" stroke="black" markerEnd="url(#arrow)" />
                     <rect x="310" y="310" width="100" height="40" fill="lightgreen" stroke="black" />
                     <text x="360" y="335" textAnchor="middle" fill="black" style={{ cursor: "pointer" }} >ADD USER</text>
 
@@ -110,13 +98,15 @@ s
                     <text x="153" y="170" textAnchor="middle" fill="black">Request ?</text>
 
                     <line x1="153" y1="190" x2="153" y2="218" stroke="black" markerEnd="url(#arrow)" />
-                    <rect x="105" y="220" width="100" height="40" fill="red" stroke="black" />
-                    <text x="155" y="245" textAnchor="middle" fill="black" style={{ cursor: "pointer" }} >
-                      Already</text>
+                    <rect x="115" y="220" width="80" height="40" fill="red" stroke="black" />
+                    <text x="155" y="245" textAnchor="middle" fill="white" style={{ cursor: "pointer" }} >Already</text>
 
+                    <rect x="185" y="305" width="90" height="45" fill="cyan" stroke="black" />
+                    <text x="230" y="325" textAnchor="middle" fill="black" style={{ cursor: "pointer" }} >Delete in<tspan x="230" dy="1em">myAccess</tspan></text>
+                   
+                    <line x1="240" y1="305" x2="265" y2="87" stroke="black" markerEnd="url(#arrow)" />
 
                     <line x1="220" y1="165" x2="220" y2="300" stroke="black" markerEnd="url(#arrow)" />
-                   
 
                     <defs>
                       <marker id="arrow" markerWidth="10" markerHeight="10" refX="6" refY="3" orient="auto">
